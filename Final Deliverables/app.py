4316ece8466059886c3ea48235a1a916cf7a3c1d
jksandy <86236260+jksandy@users.noreply.github.com>
2022-11-12 16:45:15 +0530

    Code Modified

--- Final Deliverables/app.py
+++ Final Deliverables/app.py
@@ -3,11 +3,13 @@ import os
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.inception_v3 import preprocess_input
-import requests
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request,flash, render_template, redirect,url_for
 from cloudant.client import Cloudant
+from twilio.rest import Client
 model = load_model(r"Updated-xception-diabetic-retinopathy.h5")
 app = Flask(__name__)
+app.secret_key="abc"
+app.config['UPLOAD_FOLDER'] = "User_Images"
 # Authenticate using an IAM API key
 client = Cloudant.iam('d3ffc21a-c9d1-4276-a7c3-d7a48a949e1f-bluemix',
                         'oS6rF9Lb8-d8IyJW4VEdHx5kiIN9ehQnNoj8ygKXFjzu', connect=True)
@@ -16,17 +18,17 @@ my_database = client.create_database('my_db')
 if my_database.exists():
     print("Database '{0}' successfully created.".format('my_db'))
 # default home page or route
+
+user = ""
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pred="Login", vis ="visible")
 
 @ app.route('/index')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", pred="Login", vis ="visible")
 
-'''@ app.route('/register')
-def register():
-    return render_template("register.html")'''
 
 # registration page
 @ app.route('/register',methods=["GET","POST"])
@@ -58,41 +60,39 @@ def register():
 
 @ app.route('/login', methods=['GET','POST'])
 def login():
-    if request.method == "POST":
-        user = request.form.get('name')
-        passw = request.form.get('pass')
+    if request.method == "GET":
+        user = request.args.get('mail')
+        passw = request.args.get('pass')
         print(user, passw)
-        query = {'_id': {'$eq': user}}
+        query = {'mail': {'$eq': user}}
         docs = my_database.get_query_result(query)
         print(docs)
         print(len(docs.all()))
         if (len(docs.all()) == 0):
-            return render_template('login.html', pred="The username is not found.")
+            return render_template('login.html', pred="")
         else:
-            if ((user == docs[0][0]['_id'] and passw == docs[0][0]['pswd'])):
-                return redirect(url_for('prediction'))
+            if ((user == docs[0][0]['mail'] and passw == docs[0][0]['psw'])):
+                flash("Logged in as " + str(user))
+                return render_template('index.html', pred="Logged in as "+str(user), vis ="hidden", vis2="visible")
             else:
-                print('Invalid User')
+                return render_template('login.html', pred="The password is wrong.")
     else:
         return render_template('login.html')
+
 
 @ app.route('/logout')
 def logout():
     return render_template('logout.html')
 
-@app.route("/predict")
+@app.route("/predict",methods=["GET", "POST"])
 def predict():
-    return render_template("prediction.html")
-
-@ app.route('/result', methods=["GET", "POST"])
-def res():
     if request.method == "POST":
-        f = request.files['image']
+        f = request.files['file']
         # getting the current path 1.e where app.py is present
         basepath = os.path.dirname(__file__)
         #print ( " current path " , basepath )
         # from anywhere in the system we can give image but we want that
-        filepath = os.path.join(basepath, 'uploads', f.filename)
+        filepath = os.path.join(str(basepath), 'User_Images', str(f.filename))
         #print ( " upload folder is " , filepath )
         f.save(filepath)
         img = image.load_img(filepath, target_size=(299, 299))
@@ -101,14 +101,26 @@ def res():
         #print ( x )
         img_data = preprocess_input(x)
         prediction = np.argmax(model.predict(img_data), axis=1)
-        # prediction = model.predict ( x ) #instead of predict_classes ( x ) we can use predict ( X ) ---- > predict_classes ( x ) gave error
-        # print ( " prediction is prediction )
-        index = [' No Diabetic Retinopathy ', ' Mild DR ',
-                 ' Moderate DR ', ' Severe DR ', ' Proliferative DR ']
-        # result = str ( index [ output [ 011 )
+        index = [' No Diabetic Retinopathy ', ' Mild NPDR ',
+                 ' Moderate NPDR ', ' Severe NPDR ', ' Proliferative DR ']
         result = str(index[prediction[0]])
         print(result)
-        return render_template('prediction.html', prediction=result)
+        account_sid = 'ACe84a385fa5539d372c1a924452f489a3'
+        auth_token = '359788a4ddfb510ac8ecd2fa948b924e'
+        
+        client = Client(account_sid, auth_token)
+        
+        ''' Change the value of 'from' with the number 
+        received from Twilio and the value of 'to'
+        with the number in which you want to send message.'''
+        message = client.messages.create(
+                                    from_='+17088347950',
+                                    body ='Results: '+ result,
+                                    to ='+919500680243'
+                                )
+        return render_template('prediction.html', prediction=result, fname = filepath)
+    else:
+        return render_template("prediction.html")
 
 if __name__ == "__main__":
     app.debug = True
